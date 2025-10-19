a23bcca109bac1f4e1a00c3e6b9eb828f49f4d92
sakkoumhamza <hamzasakkoum1@gmail.com>
2025-10-19 21:15:48 +0100

    Minor fixes

--- Scrapy/stages/pipelines.py
+++ Scrapy/stages/pipelines.py
@@ -31,28 +31,3 @@ class JsonWriterPipeline:
         self.first_item = False
         json.dump(item, self.file, ensure_ascii=False, indent=4)
         return item
-
-# import psycopg2
-
-# class NeonPipeline:
-#     def open_spider(self, spider):
-#         self.conn = psycopg2.connect(
-#             host="ep-old-mountain-adsihkzq-pooler.c-2.us-east-1.aws.neon.tech",
-#             dbname="jobs",
-#             user="neon_db",
-#             password="npg_Bd6AGq8uisfX",
-#             sslmode="require"
-#         )
-#         self.cur = self.conn.cursor()
-
-#     def process_item(self, item, spider):
-#         self.cur.execute("""
-#             INSERT INTO internships (title, company, location, link)
-#             VALUES (%s, %s, %s, %s)
-#         """, (item['title'], item['company'], item['location'], item['link']))
-#         self.conn.commit()
-#         return item
-
-#     def close_spider(self, spider):
-#         self.cur.close()
-#         self.conn.close()
